8381156b32d594d3600c0c6b0e7d9638db4027b9
xlt <2219321592@qq.com>
2022-08-22 22:56:37 +0800

    redisson限流注解

--- common/src/main/java/com/xlt/limiter/RequestLimiter.java
+++ common/src/main/java/com/xlt/limiter/RequestLimiter.java
@@ -3,6 +3,13 @@ package com.xlt.limiter;
 import org.redisson.api.RateIntervalUnit;
 import org.redisson.api.RateType;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.METHOD,ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
 public @interface RequestLimiter {
     String limiterName() default "";
     RateType mode() default RateType.PER_CLIENT;
