18026660fcebef98c2887b06cc87d2dea3f61e2c
xlt <2219321592@qq.com>
2023-01-08 23:46:39 +0800

    切换nacos配置中心

--- user/src/main/java/com/xlt/UserApplication.java
+++ user/src/main/java/com/xlt/UserApplication.java
@@ -1,14 +1,15 @@
 package com.xlt;
 
+import com.alibaba.nacos.spring.context.annotation.config.NacosPropertySource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import tk.mybatis.spring.annotation.MapperScan;
 
-//@SpringBootApplication(exclude = {DruidDataSourceAutoConfigure.class})
 @SpringBootApplication
 @EnableDiscoveryClient
 @MapperScan(value = "com.xlt.mapper")
+@NacosPropertySource(dataId = "user", autoRefreshed = true)
 public class UserApplication {
 
     public static void main(String[] args) {
