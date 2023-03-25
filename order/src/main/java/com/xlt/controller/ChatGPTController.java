c3536e6e466eb8b293d1b3716cc66a3ad20669af
xlt <2219321592@qq.com>
2023-03-26 00:07:34 +0800

    尝试chatGPT3

--- order/src/main/java/com/xlt/controller/ChatGPTController.java
+++ order/src/main/java/com/xlt/controller/ChatGPTController.java
@@ -3,14 +3,21 @@ package com.xlt.controller;
 import com.theokanning.openai.completion.CompletionRequest;
 import com.theokanning.openai.completion.CompletionResult;
 import com.theokanning.openai.service.OpenAiService;
+import com.xlt.config.http.AuthenticatorExt;
 import com.xlt.model.response.DataResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
+import java.net.Authenticator;
+import java.net.InetSocketAddress;
+import java.net.PasswordAuthentication;
+import java.net.Proxy;
 import java.time.Duration;
 
 @RestController
@@ -18,9 +25,11 @@ import java.time.Duration;
 @Slf4j
 public class ChatGPTController {
 
+
+
+
     @RequestMapping(value = "/question", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
     public DataResponse<String> questionChatGPT(@RequestBody String prompt) {
-
         OpenAiService service = new OpenAiService("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", Duration.ofSeconds(20));
        log.info("\nCreating completion,prompt={}",prompt);
         CompletionRequest completionRequest = CompletionRequest.builder()
@@ -36,13 +45,11 @@ public class ChatGPTController {
             resultSb.append(action.getText()).append("\n");
         });
 
-//        System.out.println("\nCreating Image...");
-//        CreateImageRequest request = CreateImageRequest.builder()
-//                .prompt("A cow breakdancing with a turtle")
-//                .build();
-//
-//        System.out.println("\nImage is located at:");
-//        System.out.println(service.createImage(request).getData().get(0).getUrl());
+        SimpleClientHttpRequestFactory httpRequestFactory = new SimpleClientHttpRequestFactory();
+        httpRequestFactory.setProxy(new Proxy(Proxy.Type.HTTP, new InetSocketAddress("8.218.219.31", 19220)));
+        RestTemplate restTemplate = new RestTemplate(httpRequestFactory);
+        Authenticator.setDefault(new AuthenticatorExt("pe68v5qBZn","bOfSPLLeGK"));
+        restTemplate.postForEntity()
         return new DataResponse<>(resultSb.toString());
     }
 }
